90e18530b5ca4dac79c257a3ca1580bcfd37cd13
alikhawar1 <34434789+alikhawar1@users.noreply.github.com>
2020-02-14 11:21:12 +0500

    Update dbo.Table_2.sql

--- Tables/dbo.Table_2.sql
+++ Tables/dbo.Table_2.sql
@@ -1,5 +1,5 @@
 CREATE TABLE [dbo].[Table_2]
 (
-[ali farag hai] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[ali samajna hai] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
